e008a79a5b00c12b47a58ac4833536abd0659854
Ilkka Jylhä <ilkka.jylha@iki.fi>
2015-09-23 20:18:44 +0300

    Change scp to rsync, support for folders

--- dropit.sh
+++ dropit.sh
@@ -6,5 +6,9 @@ REMOTE_HOST="127.0.0.1" 		 # fqdn or ip
 REMOTE_USER="$USER"      		 # Login user at remote host 
 REMOTE_URL="http://localhost:8080"	 # fqdn or ip and port, if you are using other than 80 for HTTP or 443 for HTTPS, in case you dont want to use REMOTE_HOST
 
-scp "$1" "$REMOTE_USER"@"$REMOTE_HOST:$REMOTE_DIR""$TMP_DIR_R"
+if [[ -d "$1" ]]; then
+	rsync -e ssh -r  "$1" "$REMOTE_USER"@"$REMOTE_HOST:$REMOTE_DIR""$TMP_DIR_R"/
+else
+	rsync -e ssh  "$1" "$REMOTE_USER"@"$REMOTE_HOST:$REMOTE_DIR""$TMP_DIR_R"
+fi
 echo ""$REMOTE_URL"/"$TMP_DIR_R"/"$1""
